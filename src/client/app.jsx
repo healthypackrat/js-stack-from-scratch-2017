515eef0db6030b21ccce18033599edf973eac510
healthypackrat <healthypackrat@gmail.com>
2018-07-28 16:01:03 +0900

    Use React-Redux
    $ yarn add react-redux

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,11 +1,18 @@
 // @flow
 
 import React from 'react'
+import HelloButton from './container/hello-button'
+import Message from './container/message'
+import { APP_NAME } from '../shared/config'
 
 const App = () => (
-  <h1>
-    Hello React!
-  </h1>
+  <div>
+    <h1>
+      {APP_NAME}
+    </h1>
+    <Message />
+    <HelloButton />
+  </div>
 )
 
 export default App
