a9a0b3708582449a8b449c0da2a04c7a71050df6
healthypackrat <healthypackrat@gmail.com>
2018-07-28 22:44:09 +0900

    Use React Helmet
    $ yarn add react-helmet

--- src/shared/component/page/not-found.jsx
+++ src/shared/component/page/not-found.jsx
@@ -1,11 +1,23 @@
 // @flow
 
 import React from 'react'
+import Helmet from 'react-helmet'
+
+const title = 'Page Not Found'
 
 const NotFoundPage = () => (
-  <p>
-    Page not found
-  </p>
+  <div>
+    <Helmet
+      title={title}
+      meta={[
+        { name: 'description', content: 'A page to say hello' },
+        { property: 'og:title', content: title },
+      ]}
+    />
+    <h1>
+      {title}
+    </h1>
+  </div>
 )
 
 export default NotFoundPage
