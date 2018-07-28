1a46beca77e4e60e4bdfea84bf42b99480987124
healthypackrat <healthypackrat@gmail.com>
2018-07-28 16:48:22 +0900

    Extend app with asynchronous call
    $ yarn add isomorphic-fetch
    $ yarn add redux-thunk

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -2,7 +2,9 @@
 
 import React from 'react'
 import HelloButton from './container/hello-button'
+import HelloAsyncButton from './container/hello-async-button'
 import Message from './container/message'
+import MessageAsync from './container/message-async'
 import { APP_NAME } from '../shared/config'
 
 const App = () => (
@@ -12,6 +14,8 @@ const App = () => (
     </h1>
     <Message />
     <HelloButton />
+    <MessageAsync />
+    <HelloAsyncButton />
   </div>
 )
 
